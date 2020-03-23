5d95445a87145be7ff671f9ed0a358990ca937d2
Artemut555 <zavarza555@yandex.ru>
2020-03-23 21:31:04 +0300

    new adresses written

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,10 @@
-this.videos = [{ title: "Rain", url: "http://167.172.51.41:8080/Rain.mp4" },
-                   { title: "Nature", url: "http://167.172.51.41:8080/Nature.mp4" },
-                   { title: "Fire", url: "http://167.172.51.41:8080/Fire.mp4" },
-                   { title: "Birds", url: "http://167.172.51.41:8080/Birds.mp4" },
-                   { title: "Piano", url: "http://167.172.51.41:8080/Piano.mp4" },
-		   { title: "Waterfall", url: "http://167.172.51.41:8080/Waterfall.mp4" },
-		   { title: "Wind", url: "http://167.172.51.41:8080/Wind.mp4" }];
+this.videos = [{ title: "Rain", url: "http://35.184.185.233/Rain.mp4" },
+                   { title: "Nature", url: "http://35.184.185.233/Nature.mp4" },
+                   { title: "Fire", url: "http://35.184.185.233/fireplace.mp4" },
+                   { title: "Birds", url: "http://35.184.185.233/Birds.mp4" },
+                   { title: "Piano", url: "http://35.184.185.233/Piano.mp4" },
+		   { title: "Waterfall", url: "http://35.184.185.233/Waterfall.mp4" },
+		   { title: "Wind", url: "http://35.184.185.233/Wind.mp4" }];
 
 this.rainVideoId = 0;
 this.natureVideoId = 1;
