b7e84bc1ccb3fcedde72e8cecf1b50e3753f2dc6
mplobanov <mishalob2002@yandex.ru>
2020-07-15 22:55:36 +0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,10 @@
-this.videos = [{ title: "Rain", url: "https://stingrayphotos-d79f3.web.app/Rain.mp4" },
-                   { title: "Nature", url: "https://stingrayphotos-d79f3.web.app/Nature.mp4" },
-                   { title: "Fire", url: "https://stingrayphotos-d79f3.web.app/fireplace.mp4" },
-                   { title: "Birds", url: "https://stingrayphotos-d79f3.web.app/Birds.mp4" },
-                   { title: "Piano", url: "https://stingrayphotos-d79f3.web.app/Piano.mp4" },
-		   { title: "Waterfall", url: "https://stingrayphotos-d79f3.web.app/Waterfall.mp4" },
-		   { title: "Wind", url: "https://stingrayphotos-d79f3.web.app/Wind.mp4" }];
+this.videos = [{ title: "Rain", url: "http://84.201.160.228/Videos/Rain.mp4" },
+                   { title: "Nature", url: "http://84.201.160.228/Videos/Nature.mp4" },
+                   { title: "Fire", url: "http://84.201.160.228/Videos/fireplace.mp4" },
+                   { title: "Birds", url: "http://84.201.160.228/Videos/Birds.mp4" },
+                   { title: "Piano", url: "http://84.201.160.228/Videos/Piano.mp4" },
+		   { title: "Waterfall", url: "http://84.201.160.228/Videos/Waterfall.mp4" },
+		   { title: "Wind", url: "http://84.201.160.228/Videos/Wind.mp4" }];
 
 this.rainVideoId = 0;
 this.natureVideoId = 1;
